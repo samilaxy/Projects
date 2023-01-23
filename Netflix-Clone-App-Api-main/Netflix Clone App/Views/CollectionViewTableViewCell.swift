3d8ffa62d297e86965c648322ed8745b9a12874d
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-01-23 12:36:12 +0000

    Initial commit

---
+++ Netflix-Clone-App-Api-main/Netflix Clone App/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone App
+//
+//  Created by Samuel Noye on 02/04/2022.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+ 
+}
